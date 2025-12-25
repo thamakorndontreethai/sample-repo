eb612447b521ba426a1b577553471cd9f5a8e263
thamxk <thamakhorn@gmail.com>
2025-12-25 14:59:02 +0700

    fix

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -2,11 +2,11 @@ const { add, subtract, multiply, divide } = require('../src/calculator');
 
 describe('Calculator', () => {
     test('adds 2 + 3 to equal 5', () => {
-        expect(add(2, 3)).toBe(5);
+        expect(add(2, 3)).toBe(5);  
     });
 
     test('adds -2 + -3 to equal 5', () => {
-        expect(add(-2, -3)).toBe(5);
+        expect(add(-2, -3)).toBe(-5);
     });
 
     test('subtracts 5 - 3 to equal 2', () => {
